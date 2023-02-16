8421376bde51ee99aa8357d13b771820edb68cf8
Nikita <kiryaev.work@yandex.ru>
2023-02-16 19:28:08 +0300

    WIP

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import "./style.scss";
 import "bootstrap";
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: "auto",
+  slidesPerGroupAuto: "auto",
+  spaceBetween: 40,
+});
